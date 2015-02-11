35a25a629f277281bca44eb07b1a37cca37822f0
Alice <alicegutteridge@gmail.com>
2015-02-11 14:15:42 +0000

    Member tests all pass

--- library.rb
+++ library.rb
@@ -1,4 +1,5 @@
 require 'singleton'
+require 'set'
 
 class Calendar
   include Singleton
@@ -17,6 +18,8 @@ class Calendar
 end
 
 class Book	
+  include Enumerable
+
 	def initialize(id, title, author)
 		@id = id
 		@title = title
@@ -51,8 +54,65 @@ class Book
 	def to_s
 		"#{@id}: #{@title}, by #{@author}"
 	end
+
+  # required for Enumerable module
+  def each
+    yield self
+  end
 end
 
+class Member
+  attr :library, :book_set
 
+  def initialize(name, library)
+    @name = name
+    @library = library
+    @book_set = Set.new
+  end
 
+  def get_name 
+    @name
+  end
 
+  def check_out(book)
+    @book_set.add(book)
+  end
+
+  def give_back(book)
+    @book_set.delete(book)
+  end
+
+  # for naming purposes
+  def return(book)
+    give_back(book)
+  end
+
+  def get_books
+    @book_set
+  end
+
+  def send_overdue_notice(notice)
+    message = "#{@name}: #{notice}"
+    puts message
+    message
+  end
+end
+
+class Library
+  def initialize
+    # read file, create books, dict of members
+    @today = Calendar.instance
+    @open = false
+    @current_member = nil
+  end
+
+  def open
+    if (@open) 
+      raise 'The library is already open!'
+    @today.advance
+    @open = true
+    "Today is day #{@today}"
+  end
+end
+
+end
--- library_test.rb
+++ library_test.rb
@@ -1,18 +1,19 @@
 require_relative "library"
-gem 'test-unit'
 require "test/unit"
 
 class TestLibrary < Test::Unit::TestCase
-  # self.test_order = :defined
 
   # create instance variables of objects for all tests
   def setup
   	@cal = Calendar.instance
   	@book = Book.new(1, "1984", "George Orwell")
+    @lib = Library.new
+    @mem = Member.new("Alice", @lib)
   end
 
 	# testing the Calendar class
-  def test_calendar_init
+  # must be called before all Calendar.advance methods to pass
+  def test_0_calendar_init
 		assert_equal(0, @cal.get_date)
 	end
 
@@ -27,11 +28,11 @@ class TestLibrary < Test::Unit::TestCase
 	end
 
 	def test_book_get_title
-		assert_equal('1984', @book.get_title)
+		assert_equal("1984", @book.get_title)
 	end
 
 	def test_book_get_author
-		assert_equal('George Orwell', @book.get_author)
+		assert_equal("George Orwell", @book.get_author)
 	end
 
 	def test_book_get_due_date_nil
@@ -53,4 +54,32 @@ class TestLibrary < Test::Unit::TestCase
 		assert_equal("1: 1984, by George Orwell", @book.to_s)
 	end
 
+  # testing the Member class
+  def test_member_init
+    assert_equal(@lib, @mem.library)
+  end
+
+  def test_member_get_name
+    assert_equal("Alice", @mem.get_name)
+  end
+
+  def test_member_0_no_books
+    assert(@mem.get_books.empty?)
+  end
+
+  def test_member_1_check_out
+    @mem.check_out(@book)
+    temp_set = Set.new(@book)
+    assert_equal(temp_set, @mem.get_books)
+  end
+
+  def test_member_2_give_back
+    @mem.give_back(@book)
+    assert(@mem.get_books.empty?)
+  end
+
+  def test_member_send_overdue_notice
+    assert_equal("Alice: Books are overdue", 
+      @mem.send_overdue_notice("Books are overdue"))
+  end
 end
