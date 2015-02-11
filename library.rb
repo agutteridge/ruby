65addbf7a672f7d3378794d6201eb074547ab137
Alice <alicegutteridge@gmail.com>
2015-02-11 11:13:50 +0000

    Calendar and Book - all tests pass

---
+++ library.rb
@@ -0,0 +1,53 @@
+class Calendar
+	# make into singleton!
+	def initialize
+		@days = 0
+	end
+
+	def get_date
+		@days
+	end
+
+	def advance
+		@days += 1
+	end
+end
+
+class Book	
+	def initialize(id, title, author)
+		@id = id
+		@title = title
+		@author = author
+		@due_date = nil
+	end
+
+	def get_id
+		@id
+	end
+
+	def get_title
+		@title
+	end
+
+	def get_author
+		@author
+	end
+
+	def get_due_date
+		@due_date
+	end
+
+	def check_out(due_date)
+		@due_date = due_date
+	end
+
+	def check_in
+		@due_date = nil
+	end
+
+	def to_s
+		"#{@id}: #{@title}, by #{@author}"
+	end
+end
+
+
---
+++ library_test.rb
@@ -0,0 +1,54 @@
+require_relative "library"
+require "test/unit"
+
+class TestLibrary < Test::Unit::TestCase
+
+	# create instance variables of objects for all tests
+	def setup
+		@cal = Calendar.new
+		@book = Book.new(1, '1984', 'George Orwell')
+	end
+
+	# testing the Calendar class
+	def test_calendar_init
+		assert_equal(0, @cal.get_date)
+	end
+
+	def test_calendar_advance
+		@cal.advance
+		assert_equal(1, @cal.get_date)
+	end		
+
+	# testing the Book class
+	def test_book_get_id
+		assert_equal(1, @book.get_id)
+	end
+
+	def test_book_get_title
+		assert_equal('1984', @book.get_title)
+	end
+
+	def test_book_get_author
+		assert_equal('George Orwell', @book.get_author)
+	end
+
+	def test_book_get_due_date_nil
+		assert_nil(@book.get_due_date)
+	end
+
+	def test_book_check_out
+		date = @cal.get_date + 14
+		@book.check_out(date)
+		assert_equal(date, @book.get_due_date)
+	end
+
+	def test_book_check_in
+		@book.check_in
+		assert_nil(@book.get_due_date)
+	end
+
+	def test_book_to_s
+		assert_equal("1: 1984, by George Orwell", @book.to_s)
+	end
+
+end
